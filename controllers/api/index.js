90719f9162c49bee91f38b5f5c1b0a487282df50
LaloEnri23 <126302462+LaloEnri23@users.noreply.github.com>
2023-05-25 22:46:43 -0700

    paths

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
+const userRoutes = require('./userROute');
+const postRoutes = require('./postRoute');
+const commentRoutes = require('./commentROute');
 
 router.use('/user', userRoutes);
 router.use('/post', postRoutes);
--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 const withAuth = (req, res, next) => {
     if (!req.seeion.userId){
-        res.redirect('./login');
+        res.redirect('/login');
     } else {
         next();
     }
